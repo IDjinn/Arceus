ba9f5ba9d42129314ec4e177c9163c79c082a759
IDjinn <42049518+IDjinn@users.noreply.github.com>
2024-02-20 00:21:00 -0300

    feat: dynamic cast (disabled, should be used with configuration...)

--- ArceusCore.Tests.Console/CmdTest.cs
+++ ArceusCore.Tests.Console/CmdTest.cs
@@ -1,4 +1,7 @@
-﻿using ArceusCore.Tests.Console.Entities;
+﻿using ArceusCore.Database.Attributtes;
+using ArceusCore.Tests.Console.Entities;
+using ArceusCore.Utils.Interfaces;
+using ArceusCore.Utils.Parsers;
 
 namespace ArceusCore.Tests.Console;
 
@@ -11,15 +14,61 @@ public class CmdTest
 
         Try();
     }
+    
+    
+    public readonly record struct CatalogItemId(int Id)
+    {
+        public static explicit operator int(CatalogItemId value) => value.Id;
+        public static explicit operator CatalogItemId(int id) => new(id);
+    }
+
+    public class CatalogItemConverter : IConvertible<int, CatalogItemId>
+    {
+        public CatalogItemId Parse(int source)
+        {
+            return new CatalogItemId(source);
+        }
+
+        public int Convert(CatalogItemId value)
+        {
+            return value.Id;
+        }
+    }
+    
+    [Table("catalog_items")]
+    public class CatalogItemData
+    {
+        [Column("id")]
+        [Converter(typeof(CatalogItemConverter))]
+        public CatalogItemId Id { get; init; }
+
+        [Column("item_ids")] public string ItemIds { get; init; } = null!;
+        [Column("page_id")] public uint PageId { get; init; }
+        [Column("catalog_name")] public string Name { get; init; } = null!;
+        [Column("cost_credits")] public int Credits { get; init; }
+        [Column("cost_points")] public int Points { get; init; }
+        [Column("points_type")] public int PointsType { get; init; }
+        [Column("amount")] public int Amount { get; init; }
+        [Column("limited_stack")] public int LimitedStack { get; init; }
+        [Column("limited_sells")] public int LimitedSells { get; init; }
+        [Column("order_number")] public int OrderNumber { get; init; }
+        [Column("offer_id")] public int OfferId { get; init; }
+        [Column("song_id")] public uint SongId { get; init; }
+        [Column("extradata")]
+        public string ExtraData { get; init; }
+
+        [Column("have_offer")]
+        [Converter(typeof(EnumToBoolConverter))]
+        public bool HaveOffer { get; init; }
+
+        [Column("club_only")]
+        [Converter(typeof(EnumToBoolConverter))]
+        public bool ClubOnly { get; init; }
+    }
 
     private void Try()
     {
-        var userFake = new User() { Id = 1, AuthTicket = "fake-user", IsOnline = false};
-        _arceus.Query<User>($@"
-                             UPDATE `users` SET 
-                                                `auth_ticket` = @{nameof(User.AuthTicket)}, 
-                                                `online` = @{nameof(User.IsOnline)} 
-                             WHERE `id` = @Id ",userFake);
+        var catalogItems =_arceus.Query<CatalogItemData>($@"SELECT * FROM `catalog_items`");
                              
        _arceus.Commit();
     }
--- ArceusCore/Database/Data/SqlReader.cs
+++ ArceusCore/Database/Data/SqlReader.cs
@@ -1,10 +1,19 @@
-﻿using System.Data;
+﻿// #define __CONVERT_TYPE_WITH_RUNTIME__
+using System.Data;
 using System.Reflection;
 using ArceusCore.Database.Attributtes;
 using ArceusCore.Utils.Interfaces;
 using ArceusCore.Utils.Reflection;
 
 namespace ArceusCore.Database.Data;
+
+
+public class InvalidConversionException(string message, object? value, object targetProperty, Exception exception) : Exception(message)
+{
+    public object? Value { get; init; } = value;
+    public object TargetProperty { get; init; } = targetProperty;
+    public Exception Exception { get; init;} = exception;
+}
 
 public class SqlReader<TResult> : IDisposable
 {
@@ -40,25 +49,59 @@ public class SqlReader<TResult> : IDisposable
                 foreach (var (propertyName, attributes) in propertiesWithColumn)
                 {
                     // if we dont have column attribute we just skip this property.
-                    if (!attributes.TryGetValue(typeof(ColumnAttribute), out var foundAttribute) || foundAttribute is not ColumnAttribute columnAttribute)
+                    if (!attributes.TryGetValue(typeof(ColumnAttribute), out var foundAttribute) ||
+                        foundAttribute is not ColumnAttribute columnAttribute)
                         continue;
 
+                    var propertyInfo = cache.GetPropertyInfo(typeof(TResult), propertyName);
                     var dbValue = _table[index, columnAttribute.Name];
                     var value = dbValue.Object;
+
                     foreach (var (_, attribute) in attributes)
                     {
                         if (attribute is ConverterAttribute converterAttribute)
                         {
                             var instance = cache.GetInstance(converterAttribute.Type);
-                            var method =cache.GetMethod(instance, nameof(IConvertible<string, string>.Parse));
+                            var method = cache.GetMethod(instance, nameof(IConvertible<string, string>.Parse));
                             if (!dbValue.HasValue)
                                 throw new InvalidOperationException(nameof(dbValue));
 
                             value = method.Invoke(instance, [dbValue.Object]);
                         }
                     }
-                    
-                    cache.GetPropertyInfo(typeof(TResult), propertyName).SetValue(data, value);
+
+                    try
+                    {
+                        propertyInfo.SetValue(data, value);
+                    }
+                    catch (ArgumentException argumentException)
+                    {
+#if __CONVERT_TYPE_WITH_RUNTIME__
+                        try
+                        {
+                            value = Convert.ChangeType(value, propertyInfo.PropertyType); // this method is very expensive
+                        }
+                        catch
+                        {
+                            throw new InvalidConversionException(
+                                $"Could not convert '{value}' to property '{propertyName}' of '{data}'. Expected type is '{propertyInfo.PropertyType}' got {value?.GetType()}",
+                                data, propertyName, argumentException);
+                        }
+#else
+                            throw new InvalidConversionException($"Could not convert '{value}' to property '{propertyName}' of '{data}'. Expected type is '{propertyInfo.PropertyType}' got {value?.GetType()}",
+                                data, propertyName, argumentException);
+#endif
+                    }
+                    catch (TargetException targetException)
+                    {
+                        throw new InvalidConversionException(
+                            $"Could not convert '{data}' to property '{propertyName}'. Expected type is '{propertyInfo.PropertyType}' got {value?.GetType()}",
+                            data, propertyName, targetException);
+                    }
+                    catch (Exception exception)
+                    {
+                        throw;
+                    }
                 }
 
                 _table._data.Add(data);
